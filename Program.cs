8799a359085873271d8d9301151b2535e9f170ac
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-14 21:10:29 +0200

    1,3,4,6,7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,23 +25,96 @@ namespace LV6RPPOON
             {
                 note.Show();
             }
+
+            Console.ReadKey();
             */
 
-            //zadatak 2
+            /*
+            //zadatak 3
+        
+            CareTaker MycareTaker = new CareTaker();
+            ToDoItem toDo = new ToDoItem("Važno", "Ne zaboravi predati zadatke!", new DateTime(2020, 1, 1));
+            MycareTaker.AddPreviousState(toDo.StoreState());
+            toDo.text = "Još samo jedna vježba!";
+            toDo.title = "Update o labosima";
+            MycareTaker.AddPreviousState(toDo.StoreState());
+            Console.WriteLine(toDo.ToString());
+            toDo.RestoreState(MycareTaker.Undo());
+            Console.WriteLine(toDo.ToString());
+            toDo.RestoreState(careTaker.Undo());
+            Console.WriteLine(toDo.ToString());
+            toDo.RestoreState(MycareTaker.Redo());
+            Console.WriteLine(toDo.ToString());
 
-            Box ChosenItems = new Box();
+            Console.ReadKey();
+            */
 
-            ChosenItems.AddProduct(new Product("Smartwater", 1.99));
-            ChosenItems.AddProduct(new Product("Balenciagas", 950));
-            ChosenItems.AddProduct(new Product("Sweater", 64.99));
-            ChosenItems.AddProduct(new Product("Blanket", 12.49));
+            /*
+            //zadatak 4
+            BankCareTaker MybankCareTaker = new BankCareTaker();
+            BankAccount MyAccount = new BankAccount("Amela", "Vijenac Ivana Mestrovica", 1.750);
+            BankMemento memento = new BankMemento(MyAccount);
+            MybankCareTaker.PreviousState = memento;
+            Console.WriteLine(MybankCareTaker.PreviousState.Balance.ToString());
+            MyAccount.UpdateBalance(1.200);
+            memento.AddPreviousState(MyAccount);
+            MybankCareTaker.PreviousState = memento;
+            Console.WriteLine(MybankCareTaker.PreviousState.Balance.ToString());
 
-            Iterator iterator = (Iterator)ChosenItems.GetIterator();
-            for (Product product = iterator.First(); iterator.IsDone == false; product = iterator.Next())
+            Console.ReadKey();
+            */
+
+            /*
+            //zadatak 6
+            StringChecker MyStringChecker = new StringDigitChecker();
+            MyStringChecker.SetNext(newStringUpperCaseChecker());
+            MyStringChecker.SetNext(newStringLowerCaseChecker());
+            MyStringChecker.SetNext(newStringLengthChecker());
+
+            if (MyStringChecker.Check("thisisjustatest"))
             {
-                Console.WriteLine(product.ToString());
+                Console.WriteLine("This string is correct. It has passed the test");
             }
+            else Console.WriteLine("This string is incorrect. It has failed the test");
 
+            if (MyStringChecker.Check("IWillSurvive."))
+            {
+                Console.WriteLine("This string is correct. It has passed the test");
+            }
+            else Console.WriteLine("This string is incorrect. It has failed the test");
+
+
+            Console.ReadKey();
+            */
+
+            //zadatak 7
+            /*
+            StringChecker MyStringChecker = new StringDigitChecker();
+            MyStringChecker.SetNext(newStringUpperCaseChecker());
+            MyStringChecker.SetNext(newStringLowerCaseChecker());
+            MyStringChecker.SetNext(newStringLengthChecker());
+
+            StringChecker passwordChecker = new PasswordValidator(MyStringChecker);
+
+            if (passwordChecker.Check("thisisjustatest012"))
+            {
+                Console.WriteLine("This password passed the test. This is now your password");
+            }
+            else Console.WriteLine("This password is too weak. Please try again.");
+
+            if (passwordChecker.Check("CANIUSETHIS1"))
+            {
+                Console.WriteLine("This password passed the test. This is now your password");
+            }
+            else Console.WriteLine("This password is too weak. Please try again.");
+
+            if (passwordChecker.Check("Pa55w0rDD"))
+            {
+                Console.WriteLine("This password passed the test. This is now your password");
+            }
+            else Console.WriteLine("This password is too weak. Please try again.");
+            Console.ReadKey();
+            */
         }
     }
 
