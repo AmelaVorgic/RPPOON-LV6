43873410a8e98516c0f342a6eb392c25ef618145
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-14 21:14:10 +0200

    .

---
+++ CareTaker.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV6RPPOON
+{
+    class CareTaker
+    {
+        private int i;
+        private List<Memento> previousState;
+
+        public CareTaker() { this.previousState = new List<Memento>(); i = previousState.Count - 1; }
+        public CareTaker(List<Memento> previousState)
+        {
+            this.previousState = previousState;
+            i = previousState.Count - 1;
+        }
+        public Memento Undo()
+        {
+            --i;
+            if (i < 0) return null;
+            return previousState[i];
+        }
+
+        public void AddPreviousState(Memento state) { previousState.Add(state); i = previousState.Count - 1; }
+
+        public Memento Redo()
+        {
+            ++i;
+            if (i > previousState.Count - 1) return null;
+            return previousState[i];
+        }
+    }
+}
